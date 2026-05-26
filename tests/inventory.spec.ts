765bcbe2ed32de573ef4af93bbbada9c7a321185
Shiny <go-kor@yandex.ru>
2026-05-26 10:36:50 +0400

    Extract login helper function

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from '@playwright/test';
+import { login } from '../utils/login';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://www.saucedemo.com');
 
-  await page.fill('#user-name', 'standard_user');
-  await page.fill('#password', 'secret_sauce');
-  await page.click('#login-button');
+  await login(page, 'standard_user', 'secret_sauce' );
 });
 
 test('product title is visible', async ({ page }) => {
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,12 @@
 import { test, expect } from '@playwright/test';
+import { login } from '../utils/login';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://www.saucedemo.com');
 });
 
 test('user can login', async ({ page }) => {
-  await page.fill('#user-name', 'standard_user');
-  await page.fill('#password', 'secret_sauce');
-  await page.click('#login-button');
+  await login(page, 'standard_user', 'secret_sauce');
 
   await expect(page.locator('.title')).toHaveText('Products');
   await expect(page).toHaveURL(/inventory/);
