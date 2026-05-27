5214c76a3d3b8fea01f2044b62e87a8daabf0b2f
Shiny <go-kor@yandex.ru>
2026-05-27 20:38:37 +0400

    Add remove product test with CartPage

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -4,7 +4,7 @@ import { InventoryPage } from '../pages/InventoryPage';
 import { CartPage } from '../pages/CartPage';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://www.saucedemo.com');
+await page.goto('https://www.saucedemo.com');
 });
 
 test('user can add product to cart', async ({ page }) => {
@@ -14,14 +14,14 @@ test('user can add product to cart', async ({ page }) => {
   const cartPage = new CartPage(page);
 
   await loginPage.login(
-  'standard_user',
-  'secret_sauce'
+    'standard_user',
+    'secret_sauce'
   );
 
   await inventoryPage.addFirstProductToCart();
 
   await expect(
-  inventoryPage.firstRemoveButton
+    inventoryPage.firstRemoveButton
 ).toBeVisible();
 
   await expect(
@@ -31,9 +31,35 @@ test('user can add product to cart', async ({ page }) => {
   await inventoryPage.openCart();
 
   await expect(
-    cartPage.productName).toBeVisible();
+    cartPage.productName
+  ).toBeVisible();
 });
+ 
+  test('user can remove product from cart', async ({ page }) => {
+    
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+  const cartPage = new CartPage(page);
 
-  
+  await loginPage.login (
+    'standard_user',
+    'secret_sauce'
+  );
+
+  await inventoryPage.addFirstProductToCart();
+
+  await inventoryPage.openCart();
+
+  await expect(
+    cartPage.productName).toBeVisible();
+
+  await cartPage.removeProduct();
+
+  await expect(
+    cartPage.productName
+  ).toHaveCount(0);
+  }
+)
+
 
  
