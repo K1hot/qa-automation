5eaa9e542842b0a779478065e04fe53879b169bc
Shiny <go-kor@yandex.ru>
2026-05-30 13:11:50 +0400

    Add positive and negative API tests for users endpoint

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 
 test( 'API returns user list', async ({ request }) => {
 
@@ -14,4 +14,82 @@ test( 'API returns user list', async ({ request }) => {
     console.log(body);
 
     expect(body.length).toBeGreaterThan(0);
+});
+
+test('API user has correct fields', async ({ request}) => {
+
+    const response = await request.get(
+        'https://jsonplaceholder.typicode.com/users/1'
+    );
+
+    expect(response.status()).toBe(200);
+
+    const body = await response.json();
+
+    expect(body.id).toBe(1);
+
+    expect(body.name).toBe('Leanne Graham');
+
+    expect(body.email).toContain('@');
+});
+
+test ('API can create new user', async ({ request}) =>  {
+
+    const response = await request.post(
+        'https://jsonplaceholder.typicode.com/users',
+        {
+            data: {
+                name: 'George',
+                job: 'QA Automation Engineer'
+            }
+        }
+    );
+    expect(response.status()).toBe(201);
+
+    const body = await response.json();
+
+    console.log(body);
+    expect(body.name).toBe('George');
+
+    expect(body.job).toBe(
+        'QA Automation Engineer'
+    );
+});
+
+test('API request contains headers', async ({ request }) => {
+
+    const response = await request.get(
+        'https://jsonplaceholder.typicode.com/users',
+        {
+            headers: {
+                Authorization: 'Bearer fake-token',
+                Accept: 'application/json'
+            }
+        }
+    );
+
+    expect(response.status()).toBe(200);
+});   
+
+test('API returns 404 for non existing user', async ({ request }) => {
+
+  const response = await request.get(
+    'https://jsonplaceholder.typicode.com/users/999999'
+  );
+
+  expect(response.status()).toBe(404);
+});
+
+test('API returns single user', async ({ request }) => {
+  const response = await request.get
+  ('https://jsonplaceholder.typicode.com/users/1'
+  );
+
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+
+  expect(body.id).toBe(1);
+  expect(body.name).toBe('Bret');
+  expect(body.email).toBe('Sincere@april.biz');
 });
