e45981f48c597adfed43c236a1e6d3d046644565
Shiny <go-kor@yandex.ru>
2026-06-04 18:34:18 +0400

    Add Playwright storageState authentication

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,18 +1,32 @@
+// 1. Импортируем базовые инструменты для создания тестов (test) и проверок (expect)
+
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('https://www.saucedemo.com');
+// 2. ГЛАВНАЯ МАГИЯ: Говорим Playwright, что для ВСЕХ тестов в этом файле
+// нужно взять сохраненные куки и сессию из нашего JSON-файла.
+// Браузер откроется уже в состоянии "авторизован", вводить логин и пароль не придется.
 
-  const loginPage = new LoginPage(page);
-
-  await loginPage.login('standard_user', 'secret_sauce' );
+test.use({
+  storageState: 'playwright/.auth/user.json'
 });
 
-test('product title is visible', async ({ page }) => {
-  await expect(page.locator('.title')).toHaveText('Products');
-  });
+// 3. Объявляем обычный асинхронный тест с понятным названием
 
-test('cart icon is visible', async ({ page }) => {
-await expect(page.locator('.shopping_cart_link')).toBeVisible();
+test('authorized user can open inventory page', async ({ page }) => {
+  
+  // 4. Переходим СРАЗУ на защищенную страницу каталога товаров (inventory.html).
+    // Благодаря 'storageState' сверху, сайт не выкинет нас на форму логина, а пустит внутрь
+
+    await page.goto(
+      'https://www.saucedemo.com/inventory.html'
+    )
+
+    // 5. Проверка (Assertion): Находим элемент с классом '.title' (заголовок страницы)
+    // и убеждаемся, что внутри него написан текст 'Products'.
+    // Если текст совпадает — тест успешно пройден!
+
+    await expect(
+      page.locator('.title')
+    ).toHaveText('Products');
 });
+
